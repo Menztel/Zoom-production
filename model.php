122c10fdd0b4b3a7d3dd25f2aeccf344177b15e4
Maheanuu <maheanuu.allain@gmail.com>
2022-11-11 14:05:40 +0100

    initial model-page commit

--- model.php
+++ model.php
@@ -7,6 +7,7 @@
 <body>
     <div class="main-content model-content">
     <h1>TITRE DU PROJET</h1>
+    <p>test</p>
     <span></span>
     <h3>SOUS TITRE DU PROJET</h3>
     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit obcaecati ex aliquam maiores! Sapiente, ipsum. Quis quia nam numquam dignissimos. Ullam consequuntur, quos neque perspiciatis non assumenda officia itaque sint?</p>
