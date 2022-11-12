a1e659740a7a9c5c7e9c3dd9af2b30908947fbca
Maheanuu <maheanuu.allain@gmail.com>
2022-11-12 15:21:48 +0100

    Reproduction of the model provided & gallery add in html

--- model.php
+++ model.php
@@ -7,10 +7,15 @@
 <body>
     <div class="main-content model-content">
     <h1>TITRE DU PROJET</h1>
-    <p>test</p>
     <span></span>
     <h3>SOUS TITRE DU PROJET</h3>
-    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit obcaecati ex aliquam maiores! Sapiente, ipsum. Quis quia nam numquam dignissimos. Ullam consequuntur, quos neque perspiciatis non assumenda officia itaque sint?</p>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit obcaecati ex aliquam maiores! Sapiente, ipsum. Quis quia nam numquam dignissimos. Ullam consequuntur, quos neque perspiciatis non assumenda officia itaque sint?
+        
+    </p>
+    <div class="gallery-annexes-content">
+        <img src="" alt="">
+        <img src="" alt="">
+    </div>
     </div>
     
 </body>
