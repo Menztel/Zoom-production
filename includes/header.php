1e6de7f93ddf98b6574a18e9e30fdaca6a3dda4e
Maheanuu <maheanuu.allain@gmail.com>
2022-11-14 00:36:31 +0100

    social media icons bottom of the page & font-size modif

--- includes/header.php
+++ includes/header.php
@@ -34,6 +34,7 @@
                     <a href="https://www.facebook.com/zoom.production.tahiti"><img src="images/icons/facebook.svg" alt="facebook_logo"></a>
                     <a href="https://www.instagram.com/zoom.production.tahiti"><img src="images/icons/instagram.svg" alt="instagram_logo"></a>
                     <a href="https://www.linkedin.com/company/zoom-production-tahiti"><img src="images/icons/linkedin.svg" alt="linkedin_logo"></a>
+                    <a href="https://www.tiktok.com/@zoom.production.tahiti?_t=8W7KbwLNMbS&_r=1"><img src="images/icons/tiktok.svg" alt="tiktok_logo"></a>
                 </span>
             </ul>
         </nav>
