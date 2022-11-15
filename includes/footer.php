c898c104f43cc13eaa1bfdd2b59883fb2daa09df
Maheanuu <maheanuu.allain@gmail.com>
2022-11-15 20:24:19 +0100

    Add social icons in footer & change footer copyright font-family

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,9 @@
-<footer>        
-    <p>initial commit</p>
-        <p class="copyright">Zoom Production © 2022</p>
+<footer> 
+    <ul class="foot-icons">
+        <li><a href="https://www.facebook.com/zoom.production.tahiti"><img src="images/icons/facebook.svg" alt="facebook_icon"></a></li>
+        <li><a href="https://www.instagram.com/zoom.production.tahiti"><img src="images/icons/instagram.svg" alt="instagram_icon"></a></li>
+        <li><a href="https://www.linkedin.com/company/zoom-production-tahiti"><img src="images/icons/linkedin.svg" alt="linkedin_icon"></a></li>
+        <li><a href="https://www.tiktok.com/@zoom.production.tahiti?_t=8W7KbwLNMbS&_r=1"><img src="images/icons/tiktok.svg" alt="tiktok_icon"></a></li>
+    </ul>
+    <p>Zoom Production © 2022</p>
     </footer>
