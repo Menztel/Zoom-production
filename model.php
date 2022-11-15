80f158afa86535e1f86a4e591fb5376a92fc8ae4
Maheanuu <maheanuu.allain@gmail.com>
2022-11-15 19:17:09 +0100

    Add up-arrow to return on head of the page

--- model.php
+++ model.php
@@ -2,9 +2,11 @@
 <html lang="en">
     <?php
         include('includes/head.php');
-        include('includes/header.php');
     ?>
-<body>
+<body id="model-page">
+    <?php
+         include('includes/header.php');
+    ?>
     <div class="main-content model-content">
     <h1>TITRE DU PROJET</h1>
     <span></span>
@@ -16,6 +18,11 @@
         <img src="images/annexes/neon_body/image_1.jpg" alt="">
         <img src="images/annexes/neon_body/image_2.jpg" alt="">
         <img src="images/annexes/neon_body/image_3.jpg" alt="">
+    
+    </div>
+    <div id="up-link">
+    <span id="up-arrow"><a href="#model-page"><img src="images/icons/up_arrow.png" alt="up-arrow"></a></span>
+    <p>Cliquez pour remonter</p>
     </div>
     </div>
     
