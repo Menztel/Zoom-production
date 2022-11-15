9ae5e0c7a908bf0160d0fd6e81897a6ec8da595a
Maheanuu <maheanuu.allain@gmail.com>
2022-11-15 10:57:17 +0100

    Animation changeColor open menu in header

--- index.php
+++ index.php
@@ -8,29 +8,41 @@
     <body>
         <header>
             <nav class="nav_home">
-                <div class="logo"><a href="index.php"><img src="logo_zoom.svg" alt="logo_Zoom"></a></div>
-                <div class="openMenu"><i class="fa fa-bars"></i></div>
+                <div class="logo"><a href="index.php"><img src="logo_zoom.svg"></a></div>
+                <div class="openMenu"><i class="fa fa-bars i_index"></i></div>
                 <ul class="mainMenu">
-                    <li><a href="branding_page.php">Identitée visuelle</a></li>
-                    <li><a href="photographie_page.php">Photographie</a></li>
-                    <li><a href="motion_design_page.php">Motion design</a></li>
-                    <li><a href="illustration_page.php">Illustration</a></li>
-                    <li><a href="edition_page.php">Édition</a></li>
-                    <li><a href="evenementiel_page.php">Évènementiel</a></li>
-                    <li><a href="contact_page.php">Contact</a></li>
-                    <?php
-                        if(isset($_SESSION['pseudo']) && $_SESSION['pseudo'] == "zoe.lavisse"){
-                            echo '<li><a href="deconnexion.php">Déconnexion</a></li>';
-                        }
-                    ?>
-                    
-                    <div class="closeMenu"><i class="fa fa-times"></i></div>
-                    <span class="icons">
+
+
+                <div id="drop">
+
+                <p class="dropbtn">Projets</p>
+                <img id="right_chevron" src="images/icons/right_chevron.png" alt="right chevron">
+                <div class="drop_menu">
+                <li class="dropdown-content"><a class="link-content" href="motion_design_page.php">Motion design</a></li>
+                <li class="dropdown-content"><a class="link-content" href="photographie_page.php">Photographie</a></li>
+                <li class="dropdown-content"><a class="link-content" href="evenementiel_page.php">Évènementiel</a></li>
+                <li class="dropdown-content"><a class="link-content" href="illustration_page.php">Illustration</a></li>
+                <li class="dropdown-content"><a class="link-content" href="edition_page.php">Édition</a></li>
+                <li class="dropdown-content"><a class="link-content" href="branding_page.php">Identitée visuelle</a></li>
+                </div>
+                </div>
+
+                
+
+                <li><a href="evenementiel_page.php">À propos</a></li>
+                <li><a href="contact_page.php">Contact</a></li>
+                <?php
+                    if(isset($_SESSION['pseudo']) == "zoe.lavisse"){
+                        echo '<li><a href="deconnexion.php">Déconnexion</a></li>';
+                    }
+                ?>
+                
+                <div class="closeMenu"><i class="fa fa-times"></i></div>
+                <span class="icons">
                         <a href="https://www.facebook.com/zoom.production.tahiti"><img src="images/icons/facebook.svg" alt="facebook_logo"></a>
                         <a href="https://www.instagram.com/zoom.production.tahiti"><img src="images/icons/instagram.svg" alt="instagram_logo"></a>
                         <a href="https://www.linkedin.com/company/zoom-production-tahiti"><img src="images/icons/linkedin.svg" alt="linkedin_logo"></a>
                         <a href="https://www.tiktok.com/@zoom.production.tahiti?_t=8W7KbwLNMbS&_r=1"><img src="images/icons/tiktok.svg" alt="tiktok_logo"></a>
-
                     </span>
                 </ul>
             </nav>
