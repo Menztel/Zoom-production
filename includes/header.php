e33ca284a7a974a09fab32ad2e30c5182d330092
Maheanuu <maheanuu.allain@gmail.com>
2022-12-03 22:33:30 +0100

    change class in to id & remove script

--- includes/header.php
+++ includes/header.php
@@ -1,8 +1,8 @@
 <header>
         <nav class="navbar">
             <div class="logo"><a href="index.php"><img src="logo_zoom.svg"></a></div>
-            <div class="openMenu"><i class="fa fa-bars"></i></div>
-            <ul class="mainMenu">
+            <div id="openMenu"><i class="fa fa-bars"></i></div>
+            <ul id="mainMenu">
 
 
             <div id="drop">
@@ -28,7 +28,7 @@
                 }
             ?>
             
-            <div class="closeMenu"><i class="fa fa-times"></i></div>
+            <div id="closeMenu"><i class="fa fa-times"></i></div>
             <span class="icons inverse">
                     <a href="https://www.facebook.com/zoom.production.tahiti"><img src="images/icons/facebook.svg" alt="facebook_logo"></a>
                     <a href="https://www.instagram.com/zoom.production.tahiti"><img src="images/icons/instagram.svg" alt="instagram_logo"></a>
@@ -38,5 +38,3 @@
             </ul>
         </nav>
 </header>
-
-<script src="js/script.js"></script>
