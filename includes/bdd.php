acb5206ae785769b8749b22858718ba935a28b66
Maheanuu <maheanuu.allain@gmail.com>
2023-01-25 21:37:26 +0100

    change BDD port

--- includes/bdd.php
+++ includes/bdd.php
@@ -1,7 +1,7 @@
 <?php
 try
 {
-	$bdd = new PDO('mysql:host=localhost;dbname=zoom_production;port=3306;charset=utf8', 'root', 'root');
+	$bdd = new PDO('mysql:host=localhost;dbname=zoom_production;port=3307;charset=utf8', 'root', 'root');
 }
 catch (Exception $e)
 {
