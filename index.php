885dfa63207fad6eca585957400ef810b7fbea14
Maheanuu <maheanuu.allain@gmail.com>
2023-04-10 15:41:10 +0200

    rebuild: remove unused elements & hero section with background video

--- index.php
+++ index.php
@@ -7,54 +7,9 @@
     ?>
 
     <body>
-        <header>
-            <nav class="nav_home">
-                <div class="logo"><a href="index.php"><img src="logo_zoom.svg"></a></div>
-                <div class="openMenu"><i class="fa fa-bars i_index"></i></div>
-                <ul id="mainMenu">
-
-
-                <div id="drop">
-
-                <p class="dropbtn">Projets</p>
-                <div class="drop_menu">
-                <li class="dropdown-content"><a class="link-content" href="branding_page.php">Identitée visuelle</a></li>
-                <li class="dropdown-content"><a class="link-content" href="motion_design_page.php">Motion design</a></li>
-                <li class="dropdown-content"><a class="link-content" href="photographie_page.php">Photographie</a></li>
-                <li class="dropdown-content"><a class="link-content" href="edition_page.php">Édition</a></li>
-                <li class="dropdown-content"><a class="link-content" href="evenementiel_page.php">Évènementiel</a></li>
-                </div>
-                </div>
-
-                
-                <li><a href="">À propos</a></li>
-
-                <li><a href="contact_page.php">Contact</a></li>
-                <?php
-                    if(isset($_SESSION['pseudo']) == "zoe.lavisse"){
-                        echo '<li><a href="deconnexion.php">Déconnexion</a></li>';
-                    }
-                ?>
-                
-                <div id="closeMenu"><i class="fa fa-times"></i></div>
-                <span class="icons">
-                        <a href="https://www.facebook.com/zoom.production.tahiti"><img src="images/icons/facebook.svg" alt="facebook_logo"></a>
-                        <a href="https://www.instagram.com/zoom.production.tahiti"><img src="images/icons/instagram.svg" alt="instagram_logo"></a>
-                        <a href="https://www.linkedin.com/company/zoom-production-tahiti"><img src="images/icons/linkedin.svg" alt="linkedin_logo"></a>
-                        <a href="https://www.tiktok.com/@zoom.production.tahiti?_t=8W7KbwLNMbS&_r=1"><img src="images/icons/tiktok.svg" alt="tiktok_logo"></a>
-                    </span>
-                </ul>
-            </nav>
-        </header>
-        
-        <div class="hero">
-            
-
-            
-            
-            
-        
-    </div>
+        <?php
+            include('includes/header.php');
+        ?>
     
     <ul class="social">
         <li><a href="https://www.facebook.com/zoom.production.tahiti"><img src="images/icons/facebook.svg" alt="facebook_icon"></a></li>
